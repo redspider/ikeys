13e430203da81d2a76156f60913f7d6c7fa26b59
Richard Clark <richard@redspider.co.nz>
2012-12-20 13:03:50 +1300

    Basics

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request, make_response
+from functools import update_wrapper
+from subprocess import Popen, PIPE
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+triggers = {
+    'bell': """
+say "Bing"
+    """,
+    'expose': """
+tell application "System Events" to key code 101
+    """,
+    'chrome': """
+tell application "Chrome" to activate
+    """,
+    'pycharm': """
+tell application "PyCharm" to activate
+    """,
+    'sparrow': """
+tell application "Sparrow" to activate
+    """,
+    'textual': """
+tell application "Textual" to activate
+    """,
+    'mute': """
+set volumeSettings to get volume settings
+if output muted of volumeSettings is false then
+    set volume with output muted
+else
+    set volume without output muted
+end if
+    """,
+    'lock': """
+tell application "System Events"
+    start current screen saver
+end tell
+    """
+}
+
+def nocache(f):
+    def new_func(*args, **kwargs):
+        resp = make_response(f(*args, **kwargs))
+        resp.cache_control.no_cache = True
+        return resp
+    return update_wrapper(new_func, f)
+
+@app.route("/")
+def index():
+    return render_template('panel.jinja2')
+
+@app.route("/trigger", methods=['POST'])
+@nocache
+def trigger():
+    trigger = request.form['trigger']
+    if not trigger in triggers:
+        return "Unknown trigger"
+
+    p = Popen(['osascript','-'],stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    p.communicate(triggers[trigger])
+    return "OK"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8181)
